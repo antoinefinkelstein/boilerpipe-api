c161cbee5353a3d46359b39569224fcb57c41579
Tadas Subonis <tadas.subonis@gmail.com>
2016-02-23 09:36:21 +0200

    Add an option to extract HTML with the text.

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,13 +1,20 @@
 package com.blikk.boilerpipeapi
 
+import java.io.StringReader
+
 import akka.actor._
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import de.l3s.boilerpipe.document.TextDocument
+import de.l3s.boilerpipe.sax.{BoilerpipeSAXInput, HTMLHighlighter}
+import org.xml.sax.InputSource
 import spray.json._
 import spray.json.DefaultJsonProtocol._
 import de.l3s.boilerpipe.extractors.ArticleExtractor
+
+import scala.None
 
 object Server extends App {
   implicit val system = ActorSystem()
@@ -16,17 +23,26 @@ object Server extends App {
 
   case class ExtractionRequest(
     html: Option[String],
-    url: Option[String]
+    url: Option[String],
+    extract_html: Option[Boolean]
   )
-  implicit val _ = jsonFormat2(ExtractionRequest)
+  implicit val _ = jsonFormat3(ExtractionRequest)
 
   val routes =
     path("extract") {
       post { decodeRequest { entity(as[ExtractionRequest]) { req =>
         val extractor = ArticleExtractor.getInstance()
+        val hh = HTMLHighlighter.newExtractingInstance()
         req match {
-          case ExtractionRequest(Some(html), _) => complete { extractor.getText(html) }
-          case ExtractionRequest(None, Some(url)) => complete { extractor.getText(new java.net.URL(url)) }
+          case ExtractionRequest(Some(html), _, Some(extract_html)) => complete {
+            val doc = new BoilerpipeSAXInput(new InputSource(new StringReader(html))).getTextDocument()
+            extractor.process(doc)
+            val is = new InputSource(new StringReader(html))
+            hh.process(doc, is)
+          }
+          case ExtractionRequest(None, Some(url), Some(extract_html)) => complete { hh.process(new java.net.URL(url), extractor) }
+          case ExtractionRequest(Some(html), _, _) => complete { extractor.getText(html) }
+          case ExtractionRequest(None, Some(url), _) => complete { extractor.getText(new java.net.URL(url)) }
         }
       }}}
     }
