d0b1abeb594ac2f56cd3557d3045933e9141d9d5
Tadas Subonis <tadas.subonis@gmail.com>
2016-03-02 09:30:26 +0200

    Add image extraction. Add newest deps

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -2,17 +2,20 @@ package com.blikk.boilerpipeapi
 
 import java.io.StringReader
 
+import com.kohlschutter.boilerpipe.extractors.ArticleExtractor
+import com.kohlschutter.boilerpipe.sax.{BoilerpipeSAXInput, ImageExtractor, HTMLHighlighter}
+import spray.json._
+
+import scala.collection.JavaConverters._
 import akka.actor._
+import akka.http.scaladsl.model.HttpResponse
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import de.l3s.boilerpipe.document.TextDocument
-import de.l3s.boilerpipe.sax.{BoilerpipeSAXInput, HTMLHighlighter}
 import org.xml.sax.InputSource
 import spray.json._
 import spray.json.DefaultJsonProtocol._
-import de.l3s.boilerpipe.extractors.ArticleExtractor
 
 import scala.None
 
@@ -24,25 +27,53 @@ object Server extends App {
   case class ExtractionRequest(
     html: Option[String],
     url: Option[String],
-    extract_html: Option[Boolean]
+    extract_html: Option[Boolean],
+    extract_images: Option[Boolean]
   )
-  implicit val _ = jsonFormat3(ExtractionRequest)
+  implicit val _ = jsonFormat4(ExtractionRequest)
 
   val routes =
     path("extract") {
       post { decodeRequest { entity(as[ExtractionRequest]) { req =>
-        val extractor = ArticleExtractor.getInstance()
+        val articleExtractor = ArticleExtractor.getInstance()
+        val imageExtractor = ImageExtractor.getInstance()
+
         val hh = HTMLHighlighter.newExtractingInstance()
         req match {
-          case ExtractionRequest(Some(html), _, Some(extract_html)) => complete {
+          case ExtractionRequest(Some(html), _, Some(extract_html), _) => complete {
             val doc = new BoilerpipeSAXInput(new InputSource(new StringReader(html))).getTextDocument()
-            extractor.process(doc)
+            articleExtractor.process(doc)
             val is = new InputSource(new StringReader(html))
             hh.process(doc, is)
           }
-          case ExtractionRequest(None, Some(url), Some(extract_html)) => complete { hh.process(new java.net.URL(url), extractor) }
-          case ExtractionRequest(Some(html), _, _) => complete { extractor.getText(html) }
-          case ExtractionRequest(None, Some(url), _) => complete { extractor.getText(new java.net.URL(url)) }
+
+          case ExtractionRequest(None, Some(url), Some(extract_html), _) => complete {
+            hh.process(new java.net.URL(url), articleExtractor)
+          }
+
+          case ExtractionRequest(Some(html), _, _, Some(extract_images)) => complete {
+            val doc = new BoilerpipeSAXInput(new InputSource(new StringReader(html))).getTextDocument()
+            articleExtractor.process(doc)
+
+            val is = new InputSource(new StringReader(html))
+
+            val images = imageExtractor.process(doc, is).asScala.toList.map({
+              img => img.getSrc
+            })
+            images.toJson
+          }
+
+          case ExtractionRequest(None, Some(url), _, _) => complete {
+            articleExtractor.getText(new java.net.URL(url))
+          }
+
+          case ExtractionRequest(Some(html), _, _, _) => complete {
+            articleExtractor.getText(html)
+          }
+
+          case ExtractionRequest(None, Some(url), _, _) => complete {
+            articleExtractor.getText(new java.net.URL(url))
+          }
         }
       }}}
     }
